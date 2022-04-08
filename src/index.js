f5504c876eb7e8731f6f14405a7ff5eeadefafe3
gnncjegrgr <skystar234556@gmail.com>
2022-04-09 00:02:59 +0900

    image upload

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const hpp = require('hpp');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
+const dotenv = require('dotenv');
 const vision = require('@google-cloud/vision');
 const { Translate } = require('@google-cloud/translate').v2;
 const translate = require('@vitalets/google-translate-api');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
+const multer = require('multer');
 
 const imageRouter = require('./routes/image');
+
+dotenv.config();
+
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_ACCESS_KEY_ID,
+  region: 'ap-northeast-2',
+});
 
 const app = express();
 app.use(express.json());
@@ -24,10 +36,32 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'));
 }
 
+const upload = multer({
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'awesomegnnc',
+    key(req, file, db) {
+      db(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
+    },
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 },
+});
+
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 
 const translates = new Translate();
 const client = new vision.ImageAnnotatorClient();
+
+app.get('/api/basic', async (req, res) => {
+  const [result] = await client.textDetection('./src/s.png');
+
+  console.log('Result: ');
+
+  const detections = result.textAnnotations;
+  console.log('Detections: ');
+  console.log(detections[0].boundingPoly);
+  res.json({ info: detections[0].boundingPoly });
+});
 
 app.get('/api/translate', async (req, res) => {
   const [result] = await client.textDetection('./src/s.png');
@@ -61,12 +95,10 @@ app.get('/api/translate', async (req, res) => {
   }
 });
 
-// app.get('/sub', async (req, res) => {
-//   const [result] = await client.textDetection('./src/s.png');
-//   const detections = result.textAnnotations;
-
-//   console.log(detections[0]);
-// });
+app.get('/sub', upload.array('image'), async (req, res) => {
+  console.log(req.files);
+  res.json9req.files.map((v) => v.location);
+});
 
 app.use('/image', imageRouter);
 
