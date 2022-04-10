68036b2ef045315e1eb261ed215fc214ac4c01b2
gnncjegrgr <skystar234556@gmail.com>
2022-04-11 00:01:33 +0900

    asd

--- src/routes/post.js
+++ src/routes/post.js
@@ -29,7 +29,7 @@ const upload = multer({
 
 router.post('/images', upload.array('image'), (req, res, next) => {
   // POST /post/images
-  console.log(req.body);
+  console.log(req.body._parts);
   res.json({ file: req.files });
 });
 
