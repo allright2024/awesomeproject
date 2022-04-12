166f618e83db3da5b316af034a81ee39d37cf6e7
gnncjegrgr <skystar234556@gmail.com>
2022-04-12 16:20:10 +0900

    asd

--- src/routes/post.js
+++ src/routes/post.js
@@ -51,6 +51,127 @@ router.post('/images', uploadPhoto.single('photo'), async (req, res) => {
   var _uid = req.body.uid;
   var file = req.file;
   console.log(req.query.lang);
+  var lang = req.query.lang;
+
+  let langCode;
+
+  switch (lang) {
+    case '아랍어':
+      langCode = 'ar';
+    case '벨라루스어':
+      langCode = 'be';
+    case '벵골어':
+      langCode = 'bn';
+    case '보스니아어':
+      langCode = 'bs';
+    case '불가리아어':
+      langCode = 'bg';
+    case '카탈루냐어':
+      langCode = 'ca';
+    case '중국어(간체)':
+      langCode = 'zh-CN';
+    case '중국어(번체)':
+      langCode = 'zh-TW';
+    case '코르시카어':
+      langCode = 'co';
+    case '크로아티아어':
+      langCode = 'hr';
+    case '체코어':
+      langCode = 'cs';
+    case '덴마크어':
+      langCode = 'da';
+    case '네덜란드어':
+      langCode = 'nl';
+    case '영어':
+      langCode = 'en';
+    case '핀란드어':
+      langCode = 'fi';
+    case '프랑스어':
+      langCode = 'fr';
+    case '조지아어':
+      langCode = 'ka';
+    case '독일어':
+      langCode = 'de';
+    case '그리스어':
+      langCode = 'el';
+    case '히브리어':
+      langCode = 'he';
+    case '힌디어':
+      langCode = 'hi';
+    case '몽어':
+      langCode = 'hmn';
+    case '헝가리어':
+      langCode = 'hu';
+    case '아이슬란드어':
+      langCode = 'is';
+    case '인도네시아어':
+      langCode = 'id';
+    case '아일랜드':
+      langCode = 'ga';
+    case '이탈리아어':
+      langCode = 'it';
+    case '일본어':
+      langCode = 'ja';
+    case '한국어':
+      langCode = 'ko';
+    case '리투아니아어':
+      langCode = 'lt';
+    case '룩셈부르크어':
+      langCode = 'lb';
+    case '마케도니아어':
+      langCode = 'mk';
+    case '마다가스카르어':
+      langCode = 'mg';
+    case '말레이어':
+      langCode = 'ms';
+    case '마오리어':
+      langCode = 'mi';
+    case '몽골어':
+      langCode = 'mn';
+    case '미얀마어(버마어)':
+      langCode = 'my';
+    case '네팔어':
+      langCode = 'ne';
+    case '노르웨이어':
+      langCode = 'no';
+    case '페르시아어':
+      langCode = 'fa';
+    case '포르투갈어':
+      langCode = 'pt';
+    case '루마니아어':
+      langCode = 'ro';
+    case '러시아어':
+      langCode = 'ru';
+    case '스코틀랜드 게일어':
+      langCode = 'gd';
+    case '스리랑카어':
+      langCode = 'si';
+    case '슬로바키아어':
+      langCode = 'sk';
+    case '슬로베니아어':
+      langCode = 'sl';
+    case '소말리어':
+      langCode = 'so';
+    case '스페인어':
+      langCode = 'es';
+    case '스웨덴어':
+      langCode = 'sv';
+    case '태국어':
+      langCode = 'th';
+    case '터키어':
+      langCode = 'tr';
+    case '우크라이나어':
+      langCode = 'uk';
+    case '우즈베크어':
+      langCode = 'uz';
+    case '베트남어':
+      langCode = 'vi';
+    case '웨일즈어':
+      langCode = 'cy';
+    default:
+      langCode = 'ko';
+  }
+
   if (file) {
     sharp(file.path).toFile('./uploads/' + file.filename, async function (err) {
       if (err) {
@@ -64,10 +185,10 @@ router.post('/images', uploadPhoto.single('photo'), async (req, res) => {
         const detections = result.textAnnotations;
 
         try {
-          let [translations] = await translates.translate(detections[0].description, 'en');
+          let [translations] = await translates.translate(detections[0].description, langCode);
           res.json({ translate: translations, boundingPoly: detections[0].boundingPoly });
         } catch (err) {
-          translate(detections[0].description, { to: 'en' }).then((res) => {
+          translate(detections[0].description, { to: langCode }).then((res) => {
             res.json({ translate: res.text, boundingPoly: detections[0].boundingPoly });
           });
         }
